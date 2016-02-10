a259e082020fa8d70c5a10e2aca5271d582d69a5
mikey1977 <mmi@umich.edu>
2016-02-10 11:34:13 -0800

    added global obesity average

--- d3.js
+++ d3.js
@@ -16,9 +16,26 @@ $(document).ready(function() {
         var labels = ['location_id', 'location', 'location_name', 'year', 'age_group_id', 'age_group', 'age_start', 'age_end', 'sex_id', 'sex', 'unit', 'metric', 'measure', 'mean', 'lower', 'upper'];
         var result = [];
 
-        console.log(csvData);
-      }
+        var rows = csvData.split('\n');
+        var average = 0;
+        var total = 0;
+
+        for (var i = 0; i < rows.length; i++) {
+          var individualRows = rows[i].split(',');
+
+          for (var j = 0; j < individualRows.length; j++) {
+
+            //average of global obesity rates
+            if (individualRows[j] == 'prevalence') {
+              individualRows[j+1] = +individualRows[j+1];
+              total += individualRows[j+1];
+            }
+          }
+        }
+        average = total/rows.length;
+        console.log(average);
+      };
     }
-})
+});
 
 var map = new Datamap({element: document.getElementById('container')});
